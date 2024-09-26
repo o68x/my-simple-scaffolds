9b3bdb2f8cde02671d3c57423d0ae7693438a52c
Olivier Cardinaux <ocardinaux@gmail.com>
2024-09-26 13:46:15 +0200

    chore: testing

--- scaffold.mjs
+++ scaffold.mjs
@@ -1,12 +1,11 @@
-/** @type {import('simple-scaffold').ScaffoldConfigFile} */
 export default (config) => ({
   default: {
-    templates: ["project/scaffold/**/*"],
-    output: ["src/components"],
+    templates: ["node/duplicacy/ducplicacy.mjs"],
+    output: ["."],
     data: {
       className: "myClassName",
       author: "My Name",
-      name: "duplicacy.mjs"
+      name: "duplicacy"
     },
   },
 })
