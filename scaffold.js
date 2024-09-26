094a18e7a04b2104d326d5113a149efd82e63fe1
Olivier Cardinaux <ocardinaux@gmail.com>
2024-09-26 14:03:18 +0200

    chore: debug

--- scaffold.js
+++ scaffold.js
@@ -1,11 +1,14 @@
-module.exports = (config) => ({
-  default: {
-    templates: ["node/duplicacy/ducplicacy.mjs"],
-    output: ["."],
-    data: {
-      className: "myClassName",
-      author: "My Name",
-      name: "duplicacy"
+module.exports = (config) => {
+  console.log("Config:", config)
+  return {
+    default: {
+      templates: ["node/duplicacy/ducplicacy.mjs"],
+      output: ["."],
+      data: {
+        className: "myClassName",
+        author: "My Name",
+        name: "duplicacy"
+      },
     },
-  },
-})
+  }
+}
