1272ac96049794b124fc982414c200aed7dbb245
Olivier Cardinaux <ocardinaux@gmail.com>
2024-10-01 00:01:48 +0200

    chore: try again

--- scaffold.js
+++ scaffold.js
@@ -1,7 +1,7 @@
 module.exports = {
     duplicacy: {
       name: "duplicacy",
-      templates: ["node/duplicacy/duplicacy.mjs"],
+      templates: ["duplicacy/duplicacy.mjs"],
       data: { },
     }
   }
